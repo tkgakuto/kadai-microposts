e04f6e571aafa774049dc100ddf98b6bbc4d2645
tkgakuto <s20ah198@seinan-gakuin.jp>
2019-05-10 14:15:40 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,17 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+    
+ });
 
-Route::get('signup', 'Auth\RegisterController@showRegistrationFrom')->name('signup.get');
+  Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@Login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@Logout')->name('logout.get');
 
+Route::group(['middleware'=> ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    
+});
